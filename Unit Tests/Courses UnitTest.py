8de1dd6165c7ff8d3fd82e03dc49efedacc3a721
zemui <zemui@uwm.edu>
2021-11-26 19:23:05 -0600

    2nd Commit for tests

--- Unit Tests/Courses UnitTest.py
+++ Unit Tests/Courses UnitTest.py
@@ -1,29 +1,47 @@
 from courses import Courses
 import unittest
-
-
+import courses
 class MyTestCase(unittest.TestCase):
     # tas and labs are lists
     def test_good_Set_Loc(self):
         #Pre: para is a string
         #Post Location var is set to new input, NO SIDE EFFECT
-        #input = "Library"
-        #Loc_Var = SetLoc(input)
-        self.assertFalse(Loc_Var, input, msg="Input and Loc_Var are not equal")
+        input = "Library"
+        self.loc = input
+        self.assertFalse(self.loc, input, msg="Input and Loc_Var are not equal")
 
     def test_bad_Set_Loc(self):
         #Pre: para is a string
         #Post Location var is set to new input, NO SIDE EFFECT
         with self.assertRaises(TypeError, msg="input is not a string "):
-            #Loc_Var = SetLoc(123)
+            self.loc = 123
 
-    def test_getLoc(self):
+    def test_get_good_Loc(self):
         #Precondition: loc instance variable is not null
         #Postcondition: returns a string of the location of the lab, NO SIDE EFFECT
-        Loc_var = "Library"
-        Loc = getLoc()
-        self.assertEqual(Loc_var, Loc, msg="The location from getLoc is not right")
+        self.loc = "Library"
+        GetLoc = self.loc
+        self.assertEqual("Library", GetLoc, msg="The location from getLoc is not right")
 
+    def test_get_bad_Loc(self):
+        #Pre: parameter must be a string
+        #post: loc variable is set to new input, NO SIDE EFFECT
+        self.loc = 12312
+        GetLoc = self.loc
+        self.assertEqual("Library", GetLoc, msg="The location from getLoc is not right")
+
+    def test_good_getMeetTime(self):
+        #Precondition: meetTime instance variable is not null
+        #Postcondition: returns a date of the meeting time of the lab, Side Effect
+        self.time = "12:00"
+        self.assertEqual("12:00", self.time, msg="The meet time does not match up ")
+
+    def test_bad_getMeetTime(self):
+        #Precondition: meetTime instance variable is not null
+        #Postcondition: returns a date of the meeting time of the lab, Side Effect
+        input = None
+        self.time = input
+        self.assertFalse("12:00", self.time, msg="The meet time does not match up ")
 
     def test_get_NULL_TimeAndLoc(self):
         with self.assertRaises(TypeError, msg="Time or Location is null"):
