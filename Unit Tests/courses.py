8c1ebdab38fbf895dc6d6247e1840b4db9fb1d3e
alshuyu2 <alshuyu2@uwm.edu>
2021-11-26 18:43:06 -0600

    class object

---
+++ Unit Tests/courses.py
@@ -0,0 +1,7 @@
+class Courses(object):
+    def __init__(self, instr, time, loc, tas, labs):
+        self.instr = instr
+        self.time = time
+        self.loc = loc
+        self.tas = tas
+        self.labs = labs
