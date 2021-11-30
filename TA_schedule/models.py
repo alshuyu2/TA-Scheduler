449af927bada0f15ab961c1f7d11d9129ee319da
zemui <zemui@uwm.edu>
2021-11-29 22:16:00 -0600

    another commit

---
+++ TA_schedule/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.db import models
+from .roles import Role
+
+
+class PersonalInfo(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    phone = models.CharField(max_length=10)
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.ADMIN)
+    address = models.CharField(max_length=25)
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=20)
+    instr_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return User.username
+
+class TAtoClass(models.Model):
+    class_name = models.ForeignKey(Class, on_delete=models.SET_NULL, null=True)
+    ta_name = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+class Lab(models.Model):
+    section = models.CharField(max_length=20)
+    ta_name = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+class ClassToLab(models.Model):
+    class_id = models.ForeignKey(Class, on_delete=models.SET_NULL, null=True)
+    lab_id = models.ForeignKey(Lab, on_delete=models.SET_NULL, null=True)
--- Unit Tests/Courses UnitTest.py
+++ Unit Tests/Courses UnitTest.py
@@ -1,9 +1,107 @@
-from courses import Courses
 import unittest
-import courses
-class MyTestCase(unittest.TestCase):
+from datetime import datetime
+from TA_schedule.models import Class, Lab
+
+class Courses:
     # tas and labs are lists
-    def test_good_Set_Loc(self):
+    def __init__(self, instr=None, time = None, loc = '', tas= None, labs = None):
+        self.instr = instr
+        self.meetTime = time
+        self.loc = loc
+        self.tas = tas
+        self.labs = labs
+
+    class TA:
+        pass
+
+    class Lab:
+        pass
+
+class TestInit(unittest.TestCase):
+    def setUp(self):
+        self.dt = datetime.strptime("11:30 20/05/2021", "%H:%M %d/%m/%Y")
+        self.Course = Courses()
+        self.Course1 = Courses('zac', self.dt, 'library', {"mike", "paul"}, {"CS 932"})
+
+    def test_default_instr(self):
+        self.assertIsNone(self.Course.instr)
+
+    def test_default_time(self):
+        self.assertIsNone(self.Course.time)
+
+    def test_default_loc(self):
+        self.assertEqual(self.Course.loc, '')
+
+    def test_default_tas(self):
+        self.assertIsNone(self.Course.tas)
+
+    def test_default_loc(self):
+        self.assertIsNone(self.Course.labs)
+
+    def test_instr(self):
+        self.assertEqual(self.Course1.instr, 'zac')
+
+    def test_meetTime(self):
+        self.assertEqual(self.Course1.dt, self.dt)
+
+    def test_loc(self):
+        self.assertEqual(self.Course1.loc, 'library')
+
+    def test_tas(self):
+        self.assertEqual(self.Course1.tas, {"mike", "paul"})
+
+    def test_labs(self):
+        self.assertEqual(self.Course1.labs, {"CS 932"})
+
+
+class Test_getTimeAndLoc(unittest.TestCase):
+    def setUp(self):
+        self.dt = datetime.strptime("11:30 20/05/2021", "%H:%M %d/%m/%Y")
+        self.Course1 = Courses('zac', self.dt, 'library', {"mike", "paul"}, {"CS 932"})
+
+    def test_getTimeAndLoc(self):
+        self.assertEqual(self.course1.getTimeAndLoc(), "11:30 library")
+
+
+class Test_addTas(unittest.TestCase):
+    def setUp(self):
+        self.ta = "sam"
+        self.Course1 = Courses('zac', self.dt, 'library', {"mike", "paul"}, {"CS 932"})
+
+    def test_addTas(self):
+        self.course1.addTAs(self.ta)
+        self.assertEqual(self.Course1.tas, {"mike", "paul", "sam"})
+
+class Test_removeTas(unittest.TestCase):
+    def setUp(self):
+        self.Course1 = Courses('zac', self.dt, 'library', {"mike", "paul"}, {"CS 932"})
+
+    def test_removeTa(self):
+        self.ta = "mike"
+        self.course1.removeTA(self.ta)
+
+        self.assertEqual(self.Course1.tas, {"paul"})
+
+class Test_addLab(unittest.TestCase):
+    def setUp(self):
+        self.Lab = "CA 202"
+        self.Course1 = Courses('zac', self.dt, 'library', {"mike", "paul"}, {"CS 932"})
+
+    def test_addTas(self):
+        self.course1.addLab(self.Lab)
+        self.assertEqual(self.course1.labs, {"CS 932", "CA 202"})
+
+
+class Test_removeLab(unittest.TestCase):
+    def setUp(self):
+        self.Course1 = Courses('zac', self.dt, 'library', {"mike", "paul"}, {"CS 932", "CS 212"})
+
+    def test_removeLab(self):
+        self.Lab = "CS 932"
+        self.course1.removeLab(self.Lab)
+        self.assertEqual(self.Course1.labs, {"CS 212"})
+"""
+test_good_Set_Loc(self):
         #Pre: para is a string
         #Post Location var is set to new input, NO SIDE EFFECT
         input = "Library"
@@ -45,6 +143,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_get_NULL_TimeAndLoc(self):
         with self.assertRaises(TypeError, msg="Time or Location is null"):
+            input = None
           #a = getTime(User, Date)
           #b = getLocation(User, Date)
 
@@ -56,4 +155,4 @@ class MyTestCase(unittest.TestCase):
         self.instr = instr
         #Check if it's false, if not, then do nothing since it returns a void method
         self.assertFalse(self.instr, instr, msg="The names are not the same")
-
+"""
