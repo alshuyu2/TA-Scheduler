786bc2ee6298353834b4c130e6d3c714e4da904c
Vince <houghta4@uwm.edu>
2021-12-03 16:53:36 -0600

    lots of html

---
+++ TA_schedule/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import PersonalInfo
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class PersonalInfoUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = PersonalInfo
+        fields = ['office_hours', 'phone']
+
+
