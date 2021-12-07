5287a0a79949c84163e30efe8bd9cc21e4e80e48
Vince <houghta4@uwm.edu>
2021-12-06 23:48:18 -0600

    ta tests

--- TA_schedule/TA_Class.py
+++ TA_schedule/TA_Class.py
@@ -1,24 +1,28 @@
-import string
 from datetime import datetime
+# import str
 # import instructor
 # import TA
 # import labs
 # from instructor import Instructor
-import TA_schedule.Lab
+# import TA_schedule.Lab
 
 
-class TA(object):
-    def __init__(self, name='', email='', phone='', address='',  hours=None, courses=[], labs=[]):
+class TA:
+    def __init__(self, name='', email='', phone='', address='', office_hours='', labs=None, courses=None):
+        if labs is None:
+            labs = []
+        if courses is None:
+            courses = []
         self.name = name
         self.email = email
         self.phone = phone
         self.address = address
         self.labs = labs
-        self.hours = hours
+        self.hours = office_hours
 
     def set_name(self, given_name):
-        if not isinstance(given_name, string):
-            raise TypeError("Name must be of type string")
+        # if not isinstance(given_name, str):
+        #     raise TypeError("Name must be of type str")
 
         self.name = given_name
 
@@ -26,8 +30,8 @@ class TA(object):
         return self.name
 
     def set_email(self, given_email):
-        if not isinstance(given_email, string):
-            raise TypeError("Email must be of type string")
+        # if not isinstance(given_email, str):
+        #     raise TypeError("Email must be of type str")
 
         self.email = given_email
 
@@ -35,8 +39,8 @@ class TA(object):
         return self.email
 
     def set_phone(self, given_phone):
-        if not isinstance(given_phone, string):
-            raise TypeError("Phone must be of type string")
+        # if not isinstance(given_phone, str):
+        #     raise TypeError("Phone must be of type str")
 
         self.phone = given_phone
 
@@ -44,8 +48,8 @@ class TA(object):
         return self.phone
 
     def set_address(self, given_address):
-        if not isinstance(given_address, string):
-            raise TypeError("Address must be of type string")
+        # if not isinstance(self, given_address):
+        #     raise TypeError("Address must be of type str")
         self.address = given_address
 
     def get_address(self):
@@ -55,8 +59,8 @@ class TA(object):
         return self.labs
 
     def set_hours(self, given_hours):
-        if not isinstance(given_hours, datetime):
-            raise TypeError("Argument is not of type datetime")
+        # if not isinstance(given_hours, datetime):
+        #     raise TypeError("Argument is not of type datetime")
 
         self.hours = given_hours
 
@@ -64,17 +68,17 @@ class TA(object):
         return self.hours
 
     def get_contact_info(self):
-        return "Email:" + self.get_email() + "\nPhone Number:" + self.get_phone() + "\nOffice Hours:" + self.get_hours()
+        self.get_email() + "," + self.get_phone() + "," + self.get_hours()
 
     def add_lab(self, given_lab):
         # if not isinstance(given_lab, Lab.Lab):
         #     raise TypeError("Lab must be of type lab")
         self.labs.append(given_lab)
 
-    def remove_lab(self, given_lab):
-        # if not isinstance(given_lab, Lab):
-        #     raise TypeError("Lab must be of type lab")
-        self.labs.remove(given_lab)
+    # def remove_lab(self, given_lab):
+    #     # if not isinstance(given_lab, Lab):
+    #     #     raise TypeError("Lab must be of type lab")
+    #     self.labs.remove(given_lab)
 
-    def get_course(self):
-        pass
+    # def get_course(self):
+    #     return self
