39d8db9366fe90b7dbbd145c5dab54a78a0662c1
adrianasmorales <70722509+adrianasmorales@users.noreply.github.com>
2021-12-16 14:34:35 -0600

    adjusting AddingLabsToCourses

--- TA_schedule/Acceptance_Tests/AddingLabsToCourses.py
+++ TA_schedule/Acceptance_Tests/AddingLabsToCourses.py
@@ -1,55 +1,38 @@
-from django.test import TestCase, Client
-from TA_schedule.models import PersonalInfo, Class, ClassToLab, TAtoClass, Lab, User
-from TA_schedule import views
 import unittest
+from TA_schedule import views
+from django.test import TestCase, Client
+
+from TA_schedule.forms import CourseCreateForm, LabCreateForm
+from TA_schedule.models import User, Lab, Class, ClassToLab
 
 
 # Create your tests here.
-class ClassToLabSetup(TestCase):
+class AddLabToCourse(TestCase):
     def setUp(self) -> None:
         self.user = Client()
-        self.class_lab = Class(name="Math315")
-        self.lab_ofclass = Lab(section="Lab2")
-        self.labToClass = ClassToLab(class_id=self.class_lab, lab_id=self.lab_ofclass)
+        self.info = {
+            'username': 'Adriana',
+            'password': 'adriana'
+        }
+        self.u = User.objects.create_user(**self.info)
+        self.c_info = {
+            'name': 'CS361',
+            'instr_id': self.u,
+        }
+        self.l_info = {
+            'section': 'lab 901',
+            'ta_name': self.u
+        }
+        self.u = User.objects.create_user(**self.info)
+        self.user.login(**self.info)
+        #check create course and create TA steal from other acceptance tests
+        #assign the TA to the course
 
-class ClassToLab(TestCase):
-    def test_addLab(self):
-        client = Client()
-        # verify that a user can log in
-        response = client.post("/addLabs/", {"name": "Math315", "instr_id": "", "section": "Lab2","ta_name":""}, follow=True)
-        self.assertEqual(response.status_code, 200)
+    def test_valid_course(self):
+            c_form = CourseCreateForm(data=self.c_info)
+            self.assertTrue(c_form.is_valid())
 
-    def test_addLab1(self):
-        client = Client()
-        client2 = Client()
-        # verify that a user can log in
-        response = client.post("/addLabs/", {"name": "Math315", "instr_id": "", "section": "Lab2","ta_name":""}, follow=True)
-        response2 = client2.post("/addLabs/", {"name": "Math31", "instr_id": "", "section": "Lab2", "ta_name": ""},
-                               follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response2.status_code, 200)
-
-
-    def test_addLab4(self):
-        client = Client()
-        client2 = Client()
-        response = client.post("/addLabs/", {"name": "CS351", "instr_id": "John Doe", "section": "Lab2","ta_name":""}, follow=True)
-        response2 = client2.post("/addLabs/", {"name": "English101", "instr_id": "Cathy", "section": "", "ta_name": ""},follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response2.status_code, 200)
-
-    def test_addLab5(self):
-        client = Client()
-        client2 = Client()
-        response = client.post("/addLabs/", {"name": "", "section": "Lab2","ta_name":""}, follow=True)
-        response2 = client2.post("/addLabs/", {"name": "English101", "instr_id": "Cathy", "section": "", "ta_name": ""},follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response2.status_code, 200)
-
-    def test_addLab6(self):
-        client = Client()
-        # verify that a user can log in
-        response = client.post("/addLabs/", {"name": "Englist", "instr_id": "", "section": "Lab21", "ta_name": ""},
-                               follow=True)
-        self.assertEqual(response.status_code, 200)
-
+    def test_add_new_lab(self):
+        self.user.post('/addLabs/', self.form_info, follow=True)
+        c_objects = list(ClassToLab.objects.all())
+        self.assertEqual(len(c_objects), 1)
