402b5e1ddd4a374b4a43fab5a0874ec743202e3b
alshuyu2 <alshuyu2@uwm.edu>
2021-12-16 17:23:15 -0600

    Worked on acceptance tests

--- TA_schedule/Acceptance_Tests/DeleteAccountTests.py
+++ TA_schedule/Acceptance_Tests/DeleteAccountTests.py
@@ -13,52 +13,56 @@ class Good_DeleteAcc(TestCase):
         }
         self.u = User.objects.create_user(**self.info)  # Creates a super user
         self.Admin.login(**self.info)  # used to login to the page
+        for i in range(2,6):
+            self.info['username'] = str(i)
+            User.objects.create_user(**self.info)
 
     def test_Succ_Create(self):
-        Form_info = {
-            'username': 'vinny',
-            'email': 'zack@uwm.edu',
-            'password1': 'adminadmi%aadn',
-            'password2': 'adminadmi%aadn',
-
-        }
-        # self.Admin.post("/deleteAccount/", User.objects.get(username= Form_info['username']), follow=True) #go to the deleteAcc and test it
-
-        self.Admin.post("/CreateAcc/", Form_info, follow=True)
+        # Form_info = {
+        #     'username': 'vinny',
+        #     'email': 'zack@uwm.edu',
+        #     'password1': 'adminadmi%aadn',
+        #     'password2': 'adminadmi%aadn',
+        #
+        # }
+        # # self.Admin.post("/deleteAccount/", User.objects.get(username= Form_info['username']), follow=True) #go to the deleteAcc and test it
+        #
+        # self.Admin.post("/CreateAcc/", Form_info, follow=True)
         c_objects = list(User.objects.all())
-        self.assertEqual(len(c_objects), 2)  # one for the created acc, + 1 for the Super Admin
-        self.Admin.post("/deleteAccount/", {'username': '2'}, follow=True) #go to the deleteAcc and test it
+        self.assertEqual(len(c_objects), 5)  # one for the created acc, + 1 for the Super Admin
+        self.Admin.post("/deleteAccount/", {'username': '2'}, follow=True)  # go to the deleteAcc and test it
         c_objects = User.objects.count()
-        self.assertEqual(c_objects, 1)  # minus one for the created acc
+        self.assertEqual(c_objects, 4)  # minus one for the created acc
 
+    # Another test to check if the right username has been deleted
+    # Do a post request and try to search the name for it
+    def test_delete(self):
+        self.Admin.post("/deleteAccount/", {'username': '1'}, follow=True)
+        var = list(User.objects.filter(username= 'zack'))
+        self.assertListEqual(var, [], msg="Both lists are empty")
 
-    #Another test to check if the right username has been deleted
+    def test_size(self):
+        c_objects = User.objects.count()
+        self.Admin.post("/deleteAccount/", {'username': '1'}, follow=True)
+        var = User.objects.count()
+        self.assertEqual(c_objects - 1, var, msg="it works")
 class TestDeleteForm(TestCase):
 
-    def setup(self):
+    def setUp(self) -> None:
         self.u_info = {'username': 'Vince', 'password': 'pw123'}
         User.objects.create_user(**self.u_info)
-        # self.dictionary = (self.u_info)
-        # for i in range(4):
-        #     self.u_info['username'] = str(i)
-        #     User.objects.create_user(**self.u_info)
+        for i in range(4):
+            self.u_info['username'] = str(i)
+            User.objects.create_user(**self.u_info)
 
     def test_valid_form(self):
         self.dict = {'username': '1'}
-        del_form = RemoveUserForm(self.dict)
+        del_form = RemoveUserForm({'username': '1'})
         self.assertTrue(del_form.is_valid())
 
     def test_invalid_empty_form(self):
         self.assertFalse(RemoveUserForm().is_valid(), msg='invalid form')
 
-
-
-    # def test_invalid_not_a_username(self):
-    #
-    #     self.assertFalse(RemoveUserForm({'username': '6'}).is_valid()) #check with with context if it doesn't work
-
-    # def test_invalid_one_arg(self):
-    #     self.assertFalse(RemoveUserForm({'class_name': '1'}).is_valid())
-    #
-    # def test_invalid_one_arg_other(self):
-    #     self.assertFalse(RemoveUserForm({'ta_name': '1'}).is_valid())
+    def test_not_username(self):
+        with self.assertRaises(KeyError, msg='Username is invalid'):
+            RemoveUserForm({'username': '6'}).is_valid()
